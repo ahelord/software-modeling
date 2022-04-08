b89ba5d366b2e05f79930516d68b72adf66552bb
Leonardo Rodriguez <ahelord@gmail.com>
2022-04-07 22:16:15 -0500

    chore: remove unnecesary lines

--- activity-1-polymorphism/src/test/java/ViajeTodoIncluidoTest.java
+++ activity-1-polymorphism/src/test/java/ViajeTodoIncluidoTest.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
- */
-
 import java.text.SimpleDateFormat;
 import junit.framework.TestCase;
 import org.junit.After;
