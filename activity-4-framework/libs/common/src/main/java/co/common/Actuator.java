4fd430c6089a4e0efa086f506080501e18ad9c48
Leonardo Rodriguez <ahelord@gmail.com>
2022-05-10 22:25:54 -0500

    feat: remove attribute unnecesary

--- activity-4-framework/libs/common/src/main/java/co/common/Actuator.java
+++ activity-4-framework/libs/common/src/main/java/co/common/Actuator.java
@@ -3,7 +3,6 @@ package co.common;
 import java.util.ArrayList;
 
 public abstract class Actuator {
-    protected ArrayList<Sensor> sensors;
     protected String key;
 
     public abstract boolean evalAction(Measurement measurement, Sensor sensor);
